d59aa663413d33b37a941fb2b71f5c340a051247
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2024-11-20 01:08:14 +0900

    first commit

---
+++ BooklogTests/BooklogTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import Booklog
+
+struct BooklogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
